290016a5573c7864a1766f62f508fe54202d71eb
xue <luck4268@gmail.com>
2022-02-10 14:14:24 +0800

    DO TEST

--- test.php
+++ test.php
@@ -2,13 +2,25 @@
 /*
  * @Date: 2022-02-10 13:26:33
  * @LastEditors: 曾學彥
- * @LastEditTime: 2022-02-10 14:04:43
+ * @LastEditTime: 2022-02-10 14:13:31
  */
 echo 'hi hi hi';
 echo 'yuri hiiiii';
 exit();
 echo '123123';
 out('123');
+
+
+
+add(1,1);
+add(2,2);
+out('321');
+
+
+
+
+
+
 
 function out(string $msg){
     echo $msg; 
@@ -17,5 +29,3 @@ function out(string $msg){
 function add(int $aa,int $bb){
     echo $aa + $bb; 
 }
-
-add(1,1);

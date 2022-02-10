4fe90eca63c5658d5a3203c68304fe7eadb56e88
xue <luck4268@gmail.com>
2022-02-10 14:04:57 +0800

    updata

--- test.php
+++ test.php
@@ -2,13 +2,13 @@
 /*
  * @Date: 2022-02-10 13:26:33
  * @LastEditors: 曾學彥
- * @LastEditTime: 2022-02-10 13:53:29
+ * @LastEditTime: 2022-02-10 14:04:43
  */
 echo 'hi hi hi';
 echo 'yuri hiiiii';
 exit();
 echo '123123';
-
+out('123');
 
 function out(string $msg){
     echo $msg; 

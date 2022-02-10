40891cae0f005567907c2404a16de416730e7aef
xue <luck4268@gmail.com>
2022-02-10 13:49:52 +0800

    updata

--- test.php
+++ test.php
@@ -2,6 +2,12 @@
 /*
  * @Date: 2022-02-10 13:26:33
  * @LastEditors: 曾學彥
- * @LastEditTime: 2022-02-10 13:27:12
+ * @LastEditTime: 2022-02-10 13:48:40
  */
 echo 'hi hi hi';
+echo '123123';
+
+
+function out(string $msg){
+    echo $msg; 
+}
